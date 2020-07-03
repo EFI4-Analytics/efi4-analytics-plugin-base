e5d98a10b831353880ba4c0139d47a66a8c8ecff
wwirzbicki <wwirzbicki@efigence.com>
2020-07-03 14:22:39 +0200

    added Constants

---
+++ src/main/java/com/efigence/mercury/model/segment/Constants.java
@@ -0,0 +1,9 @@
+package com.efigence.mercury.model.segment;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+    public static final String ALL_USERS_SEGMENT_NAME = "segment.allUsers";
+}
